0e66ae0228322306a3490e24ba3414620f39ac9d
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-05-04 16:40:33 -0300

    adding initial server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World!')
+import Fastify from 'fastify'
+import 'dotenv/config'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: Number(process.env.SERVER_PORT) })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
