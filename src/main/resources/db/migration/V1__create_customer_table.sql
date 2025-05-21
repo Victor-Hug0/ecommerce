538ac4dcf4051ef72eb5a193e96e6c8779bb3751
Victor-Hug0 <victorhugomourabarreto1@gmail.com>
2025-05-20 21:37:27 -0300

    adding create customer migration

---
+++ src/main/resources/db/migration/V1__create_customer_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE customers(
+
+                          id           UUID PRIMARY KEY,
+                          first_name   VARCHAR(50)  NOT NULL,
+                          last_name    VARCHAR(50)  NOT NULL,
+                          email        VARCHAR(100) NOT NULL UNIQUE,
+                          password     VARCHAR(255) NOT NULL,
+                          phone_number char(11),
+                          cpf          char(11)  NOT NULL UNIQUE,
+                          gender       CHAR(1),
+                          created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
